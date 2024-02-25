edeed12120901c14e7f07a02e635c91f6213681f
Dinesh <85795537+dinesh-git07@users.noreply.github.com>
2024-02-26 04:35:51 +0530

    Created a CRUD API (#4)
    * Sample commit
    
    * Sample commit-1
    
    * Sample commit-3
    
    * Update User.java
    
    * Update Student.java
    
    * CRUD Commit
    
    * CRUD Commit
    
    * Changes Updated
    
    ---------
    
    Co-authored-by: Dinesh <115484857+DCTPolicy@users.noreply.github.com>
    Co-authored-by: jaiganeshtrl <jaiganeshtrl@users.noreply.github.com>

--- src/test/java/com/SpringBoot/springbootRef/SpringbootRefApplicationTests.java
+++ src/test/java/com/SpringBoot/springbootRef/SpringbootRefApplicationTests.java
@@ -1,13 +1,10 @@
 package com.SpringBoot.springbootRef;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootRefApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
