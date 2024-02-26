920b7982d6ad16eb31bbf7a7e8fb8faf6f495ead
Sandhya <sankalni642@gmail.com>
2024-02-26 19:02:01 +0530

    Aggregator Microservice

---
+++ src/main/java/com/SpringBoot/springbootRef/Student/ProductController.java
@@ -0,0 +1,27 @@
+package com.SpringBoot.springbootRef.Student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/products")
+public class ProductController {
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getProducts() {
+        return productService.getProducts();
+    }
+
+    @GetMapping("/{productId}")
+    public Optional<Product> getProduct(@PathVariable("productId") Long id) {
+        return productService.getProduct(id);
+    }
+}
---
+++ src/main/java/com/SpringBoot/springbootRef/Student/ProductService.java
@@ -0,0 +1,12 @@
+package com.SpringBoot.springbootRef.Student;
+import java.util.List;
+import java.util.Optional;
+
+public class ProductService {
+    List<Product> getProducts() {
+        return null;
+    }
+    Optional<Product> getProduct(Long id) {
+        return null;
+    }
+}
