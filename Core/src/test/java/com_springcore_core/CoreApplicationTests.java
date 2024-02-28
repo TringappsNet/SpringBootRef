b924e63bd49f7e2e910b37a1f575a5333f3d3ca1
JohnsonMelvin <jmelvinmelu113@gmail.com>
2024-02-28 11:52:09 +0530

    SpringAppTest

--- Core/src/test/java/com_springcore_core/CoreApplicationTests.java
+++ Core/src/test/java/com_springcore_core/CoreApplicationTests.java
@@ -1,16 +1,20 @@
 package com_springcore_core;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class CoreApplicationTests {
 
 	@Test
 	void contextLoads() {
-		assertTrue(true);
+		ConfigurableApplicationContext context = new SpringApplicationBuilder(Config.class).build().run();
+		assertNotNull(context);
+		context.close();
 	}
 
 }
