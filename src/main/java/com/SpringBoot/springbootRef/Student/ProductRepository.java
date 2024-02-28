b2257a125124562e77e0d857a28ab447796d0745
SandhyaKalai4 <161038773+SandhyaKalai4@users.noreply.github.com>
2024-02-28 10:47:01 +0530

    Update ProductRepository.java

--- src/main/java/com/SpringBoot/springbootRef/Student/ProductRepository.java
+++ src/main/java/com/SpringBoot/springbootRef/Student/ProductRepository.java
@@ -1,6 +1,5 @@
 package com.SpringBoot.springbootRef.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< Updated upstream
 import org.springframework.stereotype.Repository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
