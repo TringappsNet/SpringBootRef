00a3480fc20066497ac666dcc2f76eb0224b5db3
AnandTringapps <129666267+AnandTringapps@users.noreply.github.com>
2024-02-29 17:38:52 +0530

    Junit Test Added

--- src/test/groovy/com/springboot/springbootref/ControllerTest.groovy
+++ src/test/groovy/com/springboot/springbootref/ControllerTest.groovy
@@ -14,6 +14,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.junit.jupiter.api.Assertions
 import org.mockito.Mockito
+ 
 /**
  * Test class for the Controller class.
  */
