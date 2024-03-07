ebc3477b1746bead4ba3bba1b0fd7ae1f96101a3
Dhaya1599 <129719964+Dhaya1599@users.noreply.github.com>
2024-03-07 10:20:29 +0530

    consumer file commit

---
+++ src/main/java/com/springboot/springbootref/consumer/kafkamessagelistener.java
@@ -0,0 +1,38 @@
+package com.springboot.springbootref.consumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.kafka.annotation.KafkaListener;
+
+
+@Service
+public class kafkamessagelistener {
+
+    Logger log = LoggerFactory.getLogger(kafkamessagelistener.class);
+
+    @KafkaListener(topics = "demo-2", groupId = "grp-1")
+    public void consume1(String message)
+    {
+        log.info("Consumer 1 received the message" + message);
+    }
+    @KafkaListener(topics = "demo-2", groupId = "grp-1")
+    public void consumer2(String message)
+    {
+        log.info("Consumer 2 received the message" + message);
+    }
+
+    @KafkaListener(topics = "demo-2", groupId = "grp-1")
+    public void consumer3(String message)
+    {
+        log.info("Consumer 3 received the message" + message);
+    }
+
+    @KafkaListener(topics = "demo-2", groupId = "grp-1")
+    public void consumer4(String message)
+    {
+        log.info("Consumer 4 received the message" + message);
+    }
+
+
+
+}
